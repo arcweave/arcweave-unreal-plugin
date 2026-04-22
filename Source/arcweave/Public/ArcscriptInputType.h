c566be154b75f91d88e972ae1ef118ae9f57d31f
salvolannister <darksawyer@hotmail.it>
2026-04-22 15:45:32 +0200

    add missing includes

--- Source/arcweave/Public/ArcscriptInputType.h
+++ Source/arcweave/Public/ArcscriptInputType.h
@@ -1,6 +1,11 @@
-
-
 #pragma once
+
+// Engine includes
+#include "CoreMinimal.h"
+
+// Generated include
+#include "ArcscriptInputType.generated.h"
+
 UENUM(BlueprintType)
 enum FArcscriptInputType {
 	CONDITION,
--- Source/arcweave/Public/ArcweaveVariable.h
+++ Source/arcweave/Public/ArcweaveVariable.h
@@ -1,5 +1,7 @@
-
 #pragma once
+
+// Engine includes
+#include "CoreMinimal.h"
 
 // Generated include
 #include "ArcweaveVariable.generated.h"
